daf8a419279c17c16fa46cbe6986415d478832db
Esra Ocak <eesraunal93@gmail.com>
2023-01-26 14:56:28 +0300

    edit

--- src/Hooks/useAuthCall.jsx
+++ src/Hooks/useAuthCall.jsx
@@ -1,11 +1,12 @@
 import axios from "axios"
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import { fetchFail, fetchStart, loginSuccess, logoutSuccess, registerSuccess } from "../features/authSlice";
 import { useDispatch } from "react-redux";
 
 const BASE_URL="https://clarusway.pythonanywhere.com/";
 
 const useAuthCall = () => {
     const dispatch= useDispatch();
+
     const login = async (userInfo) => {
         try {
             dispatch(fetchStart());
@@ -29,8 +30,21 @@ const useAuthCall = () => {
               dispatch(fetchFail());
             }
           };
+
+          const register = async (userInfo) => {
+            try {
+                dispatch(fetchStart());
+                const {data} = await axios.post(`${BASE_URL}account/auth/register/`, userInfo);
+                dispatch(registerSuccess(data));
+                
+            } catch (error) {
+                console.log(error)
+                dispatch(fetchFail());
+                
+            }
+            };
         
-          return { login, logout };
+          return { login, logout, register };
     
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,10 +8,33 @@ import Grid from "@mui/material/Grid";
 import { Link, useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
+import { TextField } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Formik,Form } from "formik";
+import * as yup from "yup";
+import useAuthCall from "../Hooks/useAuthCall";
+
+const registerSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter valid email")
+    .required("Please  enter an email"),
+  password: yup
+    .string()
+    .required("Please enter a password ")
+    .min(8, "Password must have min 8 chars")
+    .max(16, "Password must have max 16 chars")
+    .matches(/\d+/, "Password must have a number")
+    .matches(/[a-z]+/, "Password must have a lowercase")
+    .matches(/[A-Z]+/, "Password must have an uppercase")
+    .matches(/[!,?{}><%&$#£+-.]+/, " Password must have a special char"),
+});
 
 const Register = () => {
   const navigate = useNavigate();
-  const { currentUser, error } = useSelector((state) => state.auth);
+  const { currentUser, error, loading } = useSelector((state) => state.auth);
+  const {register} = useAuthCall ();
+
 
   return (
     <Container maxWidth="lg">
@@ -50,6 +73,70 @@ const Register = () => {
           >
             Register
           </Typography>
+
+          <Formik
+          initialValues={{username:"",email:"",password:""}}
+          validationSchema={registerSchema}
+          onSubmit={(values , actions )=> {
+            register(values);
+            navigate("/stock");
+            actions.resetForm();
+            actions.setSubmitting(false);
+          }}
+          >
+          {({values, isSubmitting, handleChange, handleBlur, touched, errors}) => (
+            <Form> 
+              <Box sx={{display:"flex", flexDirection:"column", gap:2}}>
+
+              <TextField
+                label="Username"
+                name="username"
+                id="username"
+                type="string"
+                variant="outlined"
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.username && Boolean(errors.username)}
+                helperText={touched.username && errors.username}/>
+
+                <TextField
+                label="Email"
+                name="email"
+                id="email"
+                type="email"
+                variant="outlined"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.email && Boolean(errors.email)}
+                helperText={touched.email && errors.email}/>
+
+                <TextField
+                label="Password"
+                name="password"
+                id="password"
+                type="password"
+                variant="outlined"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}/>
+
+                <LoadingButton 
+                type="submit" 
+                loading={loading} 
+                loadingPosition="center"
+                variant="contained">
+                  Submit
+                </LoadingButton>
+
+              </Box>
+            </Form>
+
+          )}
+          </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/">Do you have an account?</Link>
